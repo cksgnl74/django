d7c25e949fcb989946d47cb6f3ff00f60e9a1a5f
chanwhi <cksgnl74@naver.com>
2021-11-24 04:56:30 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
     # author
-    author = models.ForeignKey(User, on_delete=models.CASCADE )
+    author = models.ForeignKey(User,  null=True, on_delete=models.SET_NULL )
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
