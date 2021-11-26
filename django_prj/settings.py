65061119ab720d6241b77e9d41dd7eeeaf8f5298
chanwhi <cksgnl74@naver.com>
2021-11-27 01:45:34 +0900

    django-crispy-forms 적용

--- django_prj/settings.py
+++ django_prj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'blog',
     'single_pages',
     'django_extensions',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
