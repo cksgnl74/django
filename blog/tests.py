81d5428b400eb94615799deb09d8e0abda761a11
chanwhi <cksgnl74@naver.com>
2021-11-29 00:06:29 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase , Client
 from bs4 import BeautifulSoup
-from .models import Post,Category, Tag
+from .models import Post,Category, Tag, Comment
 from django.contrib.auth.models import User
 
 class TestView(TestCase):
@@ -28,6 +28,8 @@ class TestView(TestCase):
         self.post_003 = Post.objects.create( title='세 번째 포스트입니다', content='메렁?',author=self.user_obama )
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create( post=self.post_001, author=self.user_obama, content ='첫 번째 댓글입니다. ')
 
     def test_tag_page(self):
         response = self.client.get(self.tag_hello.get_absolute_url())
@@ -171,6 +173,13 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        #comment
+
+        comment_area = soup.find('div',id='comment-area')
+        comment_001_area = comment_area.find('div',id='comment-1')
+        #self.assertIn(self.comment_001.author.username , comment_001_area.text)
+        self.assertIn(self.comment_001.content , comment_001_area.text)
 
     def test_create_post(self):
         response = self.client.get('/blog/create_post/')
