a9ea5c9ed9d06c6009163bfb2bacb86833e9c6ec
chanwhi <cksgnl74@naver.com>
2021-12-03 15:08:43 +0900

    디자인 변경

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        labels = {'content': '내용'}
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+
+        self.fields['content'].widget.attrs = {
+            'class': 'form-control',
+            'placeholder': "내용을 입력하세요.",
+            'rows': 3
+        }
